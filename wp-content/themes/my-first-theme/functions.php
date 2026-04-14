942e45f734aadc32f45e5be0b99b48573a4a6315
csp <534662311@qq.com>
2026-04-14 21:37:31 +0800

    新增了pre_get_posts钩子，代替wp_query,多场景下分页情况

--- wp-content/themes/my-first-theme/functions.php
+++ wp-content/themes/my-first-theme/functions.php
@@ -2,4 +2,14 @@
 function my_first_theme_enqueue_assets() {
     wp_enqueue_style('my-first-theme-style', get_stylesheet_uri(), array(), '1.0.0');
 }
+function my_first_modify_main_query( $query ) {
+	if ( ! is_admin() && $query->is_main_query() ) {
+		if ( is_home() ) {
+			$query->set( 'posts_per_page', 5 );
+		} elseif ( is_category() ) {
+			$query->set( 'posts_per_page', 8 );
+		}
+	}
+}
+add_action('pre_get_posts', 'my_first_modify_main_query');
 add_action('wp_enqueue_scripts', 'my_first_theme_enqueue_assets');
